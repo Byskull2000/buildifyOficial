0f589a03f67e6be0020ef010eab45d76794e386a
Byskull2000 <grajedadiego1@gmail.com>
2024-09-22 15:39:45 -0400

    Aclaracion en el page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,6 +9,8 @@ export default function Home() {
   return (
     <div className={poppins.className}>
       <h1>PAGINA DE INICIO: page.tsx</h1>
+      <h1>Los componentes, iran en: components</h1>
+      <h1>Las paginas de navegacion, dentro de pages</h1>
     </div>
   );
 }
