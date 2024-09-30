31c2ef1cc71fcf77fc6bd57f104fdc4833107601
Jhunior Garcia <jhuniorremberto1@gmail.com>
2024-09-30 17:38:32 -0400

    Integracion Con Servicios de Google

--- frontend/src/components/ButtonGoogle.tsx
+++ frontend/src/components/ButtonGoogle.tsx
@@ -1,7 +1,14 @@
+import { useGoogleLogin } from "@react-oauth/google";
 import GoogleLogo from "../assets/google.svg";
+
 const ButtonGoogle = () => {
+    const login = useGoogleLogin({
+        onSuccess: (response) => console.log(response),
+        onError: (error) => console.log("Error en el Login: ", error)
+    })
+
     return (
-        <button className="flex items-center justify-center bg-black text-white rounded-md p-2 gap-2 w-80">
+        <button className="flex items-center justify-center bg-black text-white rounded-md p-2 gap-2 w-80" onClick={()=> login()}>
             <img src={GoogleLogo} alt="Google Logo" className="h-8" />
             Continuar con Google
         </button>
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -5,9 +5,11 @@ import Login from './pages/Login.tsx'
 import './index.css'
 import { BrowserRouter as Router,Routes, Route } from 'react-router-dom'
 import Register from './pages/Register.tsx'
+import { GoogleOAuthProvider } from '@react-oauth/google'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+    <GoogleOAuthProvider clientId='707624544355-tt3o1jqfblee1ciqmcvu2plhuitc26b7.apps.googleusercontent.com'>
     <Router>
       <Routes>
         <Route path="/" element={<App />} />
@@ -15,5 +17,6 @@ createRoot(document.getElementById('root')!).render(
         <Route path="/register" element={<Register/>} />
       </Routes>
     </Router>
+    </GoogleOAuthProvider>
   </StrictMode>,
 )
