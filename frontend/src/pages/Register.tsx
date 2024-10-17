7d6d2466d1ee0f01df1d33384db6008bc2e11759
Byskull2000 <grajedadiego1@gmail.com>
2024-10-17 16:35:51 -0400

    Control de cantidad de caracteres en el Register

--- frontend/src/pages/Register.tsx
+++ frontend/src/pages/Register.tsx
@@ -178,6 +178,7 @@ const Page = () => {
                                         id="name"
                                         name="name"
                                         pattern="[A-Za-z\s]+"
+                                        maxLength={50}
                                         className="bg-gray-100 mt-1 p-2 w-full border rounded-md focus:border-gray-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-300 transition-colors duration-300"
                                     />
                                 </div>
