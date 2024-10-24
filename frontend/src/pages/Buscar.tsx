a186712f6ab935933fb958a3ccc273f542ce043b
Jhunior Garcia <jhuniorremberto1@gmail.com>
2024-10-24 12:58:04 -0400

    Buscador implementado

--- frontend/src/pages/Buscar.tsx
+++ frontend/src/pages/Buscar.tsx
@@ -1,68 +1,85 @@
 import { useEffect, useState } from "react";
 import NavBar from "../components/NavBar";
-
-import material1 from "../assets/material1.png";
-import material2 from "../assets/material2.png";
-import material3 from "../assets/material3.png";
-import material4 from "../assets/material4.png";
-import material5 from "../assets/material5.png";
 import type { MaterialProp } from "../components/Material";
 import Material from "../components/Material";
-
+import { URL_BACKEND } from "../constant/url";
+import Loading from "../components/Loading";
+import { useLocation } from "react-router-dom";
 const Buscar = () => {
-    const searchQuery = new URLSearchParams(location.search).get("query");
     const [resultados, setResultados] = useState<MaterialProp[]>([]);
-    useEffect(() => {
-        const resultados = [
-            {
-                id: 1,
-                imagenUrl: material1,
-                precio: 200,
-                titulo: "Ladrillos 6 huecos",
-            },
-            {
-                id: 2,
-                imagenUrl: material2,
-                precio: 200,
-                titulo: "Ladrillos 6 huecos",
-            },
-            {
-                id: 3,
-                imagenUrl: material3,
-                precio: 200,
-                titulo: "Ladrillos 6 huecos",
-            },
-            {
-                id: 4,
-                imagenUrl: material4,
-                precio: 200,
-                titulo: "Ladrillos 6 huecos",
-            },
-            {
-                id: 5,
-                imagenUrl: material5,
-                precio: 80,
-                titulo: "Arena lavada",
-            },
-        ];
+    const [loading, setLoading] = useState<boolean>(false);
 
-        setResultados(resultados);
-    }, []);
+    const location = useLocation();
+    const searchQuery = new URLSearchParams(window.location.search).get(
+        "query"
+    );
+    useEffect(() => {
+        const obtenerMateriales = async () => {
+            if (!searchQuery) {
+                setResultados([]);
+                return;
+            }
+            setLoading(true);
+
+            try {
+                const body = {
+                    nombre_material: searchQuery,
+                };
+                const res = await fetch(
+                    URL_BACKEND + "/api/materiales/buscar_avanzado",
+                    {
+                        method: "POST",
+                        headers: {
+                            "Content-Type": "application/json",
+                        },
+                        body: JSON.stringify(body),
+                    }
+                );
+
+                if (!res.ok) {
+                    setResultados([]);
+                    throw new Error("Error al obtener materiales");
+                }
+
+                const { data } = await res.json();
+                setResultados(data);
+            } catch (err) {
+                console.error(err);
+            } finally {
+                setLoading(false);
+            }
+        };
+
+        obtenerMateriales();
+    }, [location, searchQuery]);
 
     return (
         <div>
             <NavBar buscar={searchQuery?.toString()} />
 
-            <div className="md:w-[90%] mx-auto">
+            <div className="md:w-[90%] mx-auto ">
                 <h1 className="text-xl font-semibold my-3">
                     Resultados de la busqueda "{searchQuery}"
                 </h1>
 
-                <div className="grid grid-cols-[repeat(auto-fit,minmax(200px,1fr))] gap-4 place-items-center">
-                    {resultados.map((material) => (
-                        <Material key={material.id} material={material} />
-                    ))}
-                </div>
+                {loading ? (
+                    <Loading />
+                ) : (
+                    <div className="grid grid-cols-[repeat(auto-fit,minmax(200px,1fr))] gap-4 place-items-center">
+                        {resultados.length > 0 ? (
+                            resultados.map((material) => (
+                                <Material
+                                    key={material.id_material}
+                                    material={material}
+                                />
+                            ))
+                        ) : (
+                            <h2 className="text-xl">
+                                No se encontraron resultados
+                            </h2>
+                        )}
+                    </div>
+                )}
             </div>
         </div>
     );
