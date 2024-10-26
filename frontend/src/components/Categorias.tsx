12fadfd008104f7e025e6fd7a35542923abec2e7
Enrrique <kikevela1811@gmail.com>
2024-10-26 02:51:26 -0400

    Barra de categorias y navegacion entre categorias

--- frontend/src/components/Categorias.tsx
+++ frontend/src/components/Categorias.tsx
@@ -1,134 +1,95 @@
 import Herramientas from "../assets/Herramientas.png";
-import Carpinteria from "../assets/Carpinteria.png";
+import Madera from "../assets/Madera.png";
 import Ladrillo from "../assets/Ladrillo.png";
 import Cemento from "../assets/Cemento.png";
-import Tablones from "../assets/Tablones.png"
-import Vigas from "../assets/Vigas.png"
-import Arena from "../assets/Arena.png"
-import Mezclas from "../assets/Mezclas.png"
-import Tejas from "../assets/Tejas.png"
-import Piedras from "../assets/Piedras.png"
-import Yeso from "../assets/Yeso.png"
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faChevronLeft, faChevronRight } from '@fortawesome/free-solid-svg-icons';
+import Tablones from "../assets/Tablones.png";
+import Vigas from "../assets/Vigas.png";
+import Arena from "../assets/Arena.png";
+import Mezclas from "../assets/Mezclas.png";
+import Tejas from "../assets/Tejas.png";
+import Piedras from "../assets/Piedras.png";
+import Yeso from "../assets/Yeso.png";
 
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
+import { useRef } from "react";
 
-const Page = () => {
-    return (
-        <div className="relative py-5">
-            <button
-                className="absolute left-0 top-1/2 -translate-y-1/2 z-50 flex items-center justify-center w-10 h-10 bg-transparent focus:outline-none"
-                onClick={() => {
-                    const scrollable = document.querySelector('.overflow-x-auto');
-                    scrollable?.scrollBy({ left: -500, behavior: 'smooth' });
-                }}
-            >
-                <FontAwesomeIcon icon={faChevronLeft} className="text-gray-700" />
-            </button>
+const categoriasData = [
+  { id: 1, name: "Ladrillo", imgSrc: Ladrillo },
+  { id: 2, name: "Cemento", imgSrc: Cemento },
+  { id: 3, name: "Tablones", imgSrc: Tablones },
+  { id: 4, name: "Vigas", imgSrc: Vigas },
+  { id: 5, name: "Arena", imgSrc: Arena },
+  { id: 6, name: "Mezclas", imgSrc: Mezclas },
+  { id: 7, name: "Herramientas", imgSrc: Herramientas },
+  { id: 8, name: "Madera", imgSrc: Madera },
+  { id: 9, name: "Tejas", imgSrc: Tejas },
+  { id: 10, name: "Yeso", imgSrc: Yeso },
+  { id: 11, name: "Piedras", imgSrc: Piedras },
+];
 
-            <div className="flex overflow-x-auto hide-scrollbar py-5 gap-4">
-                <Link to="/">
-                    <div className="w-24 sm:w-24 md:w-28 xl:w-48 bg-[#FDBC3F] border border-gray-200 rounded-lg shadow hover:bg-[#f7a914] transition-colors">
-                        <div className="flex flex-col items-center pb-10 mt-4">
-                            <img className="w-16 sm:w-16 md:w-16 xl:w-28 h-auto mb-3 rounded-full shadow-lg" src={Ladrillo} />
-                            <span className="text-sm">Ladrillos</span>
-                        </div>
-                    </div>
-                </Link>
-                <Link to="/">
-                    <div className="w-24 sm:w-24 md:w-28 xl:w-48 bg-[#FDBC3F] border border-gray-200 rounded-lg shadow hover:bg-[#f7a914] transition-colors">
-                        <div className="flex flex-col items-center pb-10 mt-4">
-                            <img className="w-16 sm:w-16 md:w-16 xl:w-28 h-auto mb-3 rounded-full shadow-lg" src={Carpinteria} />
-                            <span className="text-sm">Madera</span>
-                        </div>
-                    </div>
-                </Link>
-                <Link to="/">
-                    <div className="w-24 sm:w-24 md:w-28 xl:w-48 bg-[#FDBC3F] border border-gray-200 rounded-lg shadow hover:bg-[#f7a914] transition-colors">
-                        <div className="flex flex-col items-center pb-10 mt-4">
-                            <img className="w-16 sm:w-16 md:w-16 xl:w-28 h-auto mb-3 rounded-full shadow-lg" src={Cemento} />
-                            <span className="text-sm">Cemento</span>
-                        </div>
-                    </div>
-                </Link>
-                <Link to="/">
-                    <div className="w-24 sm:w-24 md:w-28 xl:w-48 bg-[#FDBC3F] border border-gray-200 rounded-lg shadow hover:bg-[#f7a914] transition-colors">
-                        <div className="flex flex-col items-center pb-10 mt-4">
-                            <img className="w-16 sm:w-16 md:w-16 xl:w-28 h-auto mb-3 rounded-full shadow-lg" src={Herramientas} />
-                            <span className="text-sm">Herramientas</span>
-                        </div>
-                    </div>
-                </Link>
-                <Link to="/">
-                    <div className="w-24 md:w-28 xl:w-48 bg-[#FDBC3F] border border-gray-200 rounded-lg shadow hover:bg-[#f7a914] transition-colors">
-                        <div className="flex flex-col items-center pb-10 mt-4">
-                            <img className="w-16 sm:w-16 md:w-16 xl:w-28 h-auto mb-3 rounded-full shadow-lg" src={Tablones} />
-                            <span className="text-sm">Tablones</span>
-                        </div>
-                    </div>
-                </Link>
-                <Link to="/">
-                    <div className="w-24 sm:w-24 md:w-28 xl:w-48 bg-[#FDBC3F] border border-gray-200 rounded-lg shadow hover:bg-[#f7a914] transition-colors">
-                        <div className="flex flex-col items-center pb-10 mt-4">
-                            <img className="w-16 sm:w-16 md:w-16 xl:w-28 h-auto mb-3 rounded-full shadow-lg" src={Vigas} />
-                            <span className="text-sm">Vigas</span>
-                        </div>
-                    </div>
-                </Link>
-                <Link to="/">
-                    <div className="w-24 sm:w-24 md:w-28 xl:w-48 bg-[#FDBC3F] border border-gray-200 rounded-lg shadow hover:bg-[#f7a914] transition-colors">
-                        <div className="flex flex-col items-center pb-10 mt-4">
-                            <img className="w-16 sm:w-16 md:w-16 xl:w-28 h-auto mb-3 rounded-full shadow-lg" src={Arena} />
-                            <span className="text-sm">Arena</span>
-                        </div>
-                    </div>
-                </Link>
-                <Link to="/">
-                    <div className="w-24 sm:w-24 md:w-28 xl:w-48 bg-[#FDBC3F] border border-gray-200 rounded-lg shadow hover:bg-[#f7a914] transition-colors">
-                        <div className="flex flex-col items-center pb-10 mt-4">
-                            <img className="w-16 sm:w-16 md:w-16 xl:w-28 h-auto mb-3 rounded-full shadow-lg" src={Mezclas} />
-                            <span className="text-sm">Mezclas</span>
-                        </div>
-                    </div>
-                </Link>
-                <Link to="/">
-                    <div className="w-24 sm:w-24 md:w-28 xl:w-48 bg-[#FDBC3F] border border-gray-200 rounded-lg shadow hover:bg-[#f7a914] transition-colors">
-                        <div className="flex flex-col items-center pb-10 mt-4">
-                            <img className="w-16 sm:w-16 md:w-16 xl:w-28 h-auto mb-3 rounded-full shadow-lg" src={Tejas} />
-                            <span className="text-sm">Tejas</span>
-                        </div>
-                    </div>
-                </Link>
-                <Link to="/">
-                    <div className="w-24 sm:w-24 md:w-28 xl:w-48 bg-[#FDBC3F] border border-gray-200 rounded-lg shadow hover:bg-[#f7a914] transition-colors">
-                        <div className="flex flex-col items-center pb-10 mt-4">
-                            <img className="w-16 sm:w-16 md:w-16 xl:w-28 h-auto mb-3 rounded-full shadow-lg" src={Yeso} />
-                            <span className="text-sm">Yeso</span>
-                        </div>
-                    </div>
-                </Link>
-                <Link to="/">
-                    <div className="w-24 sm:w-24 md:w-28 xl:w-48 bg-[#FDBC3F] border border-gray-200 rounded-lg shadow hover:bg-[#f7a914] transition-colors">
-                        <div className="flex flex-col items-center pb-10 mt-4">
-                            <img className="w-16 sm:w-16 md:w-16 xl:w-28 h-auto mb-3 rounded-full shadow-lg" src={Piedras} />
-                            <span className="text-sm">Piedras</span>
-                        </div>
-                    </div>
-                </Link>
-            </div>
-            <button
-                className="absolute right-0 top-1/2 -translate-y-1/2 z-50 flex items-center justify-center w-10 h-10 bg-transparent focus:outline-none"
-                onClick={() => {
-                    const scrollable = document.querySelector('.overflow-x-auto');
-                    scrollable?.scrollBy({ left: 500, behavior: 'smooth' });
-                }}
-            >
-                <FontAwesomeIcon icon={faChevronRight} className="text-gray-700" />
-            </button>
+const Categorias = () => {
+  const scrollRef = useRef<HTMLDivElement>(null);
+  const location = useLocation();
+  const currentTypeId = new URLSearchParams(location.search).get("id");
+
+  const isInicio = location.pathname === "/";
+
+  const scrollLeft = () => {
+    if (scrollRef.current) scrollRef.current.scrollLeft -= 150;
+  };
+
+  const scrollRight = () => {
+    if (scrollRef.current) scrollRef.current.scrollLeft += 150;
+  };
+
+  return (
+    <div className="relative">
+      {/* Título elegante de la sección de categorías, solo en la página de navegación */}
+      {!isInicio && (
+        <div className="text-center text-xl font-semibold mb-4 p-2 bg-[#FDBC3F] text-white rounded-md shadow-md">
+          Explore nuestra selección de materiales por categoría
         </div>
+      )}
 
-    );
+      <button
+        className="absolute left-0 top-1/2 transform -translate-y-1/2 bg-[#e3961b] text-white rounded-full w-10 h-10 flex items-center justify-center shadow-lg focus:outline-none"
+        onClick={scrollLeft}
+      >
+        ◀
+      </button>
+      <div
+        className="flex overflow-x-auto hide-scrollbar py-5 gap-4"
+        ref={scrollRef}
+      >
+        {categoriasData.map((categoria) => (
+          <Link to={`/materiales?id=${categoria.id}`} key={categoria.id}>
+            <div
+              className={`w-24 sm:w-24 md:w-28 xl:w-48 bg-[#FDBC3F] border border-gray-200 rounded-lg shadow hover:bg-[#f7a914] transition-colors ${
+                !isInicio && currentTypeId !== String(categoria.id)
+                  ? "opacity-60"
+                  : "opacity-100"
+              }`}
+            >
+              <div className="flex flex-col items-center pb-10 mt-4">
+                <img
+                  className="w-16 sm:w-16 md:w-16 xl:w-28 h-auto mb-3 rounded-full shadow-lg"
+                  src={categoria.imgSrc}
+                  alt={categoria.name}
+                />
+                <span className="text-sm">{categoria.name}</span>
+              </div>
+            </div>
+          </Link>
+        ))}
+      </div>
+      <button
+        className="absolute right-0 top-1/2 transform -translate-y-1/2 bg-[#e3961b] text-white rounded-full w-10 h-10 flex items-center justify-center shadow-lg focus:outline-none"
+        onClick={scrollRight}
+      >
+        ▶
+      </button>
+    </div>
+  );
 };
 
-export default Page;
+export default Categorias;
