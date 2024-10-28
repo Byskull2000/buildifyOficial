e4d4fb9fb56c204f06cfa7326e0062e4581536d5
Byskull2000 <grajedadiego1@gmail.com>
2024-10-28 17:36:02 -0400

    Xd

--- frontend/src/pages/Propios.tsx
+++ frontend/src/pages/Propios.tsx
@@ -15,7 +15,7 @@ const propios = () => {
     return (
         <div className="font-nunito">
             <NavBar />
-            <h1 className="font-bold text-2xl text-gray-800 mt-4 mb-6 ml-[2%]">Todas publicaciones:</h1>
+            <h1 className="font-bold text-2xl text-gray-800 mt-4 mb-6 ml-[2%]">Todas tus publicaciones:</h1>
             <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-6 px-4 lg:px-8 justify-items-center">
                 {materiales.map((material) => (
                     <Material key={material.id_material} material={material} />
