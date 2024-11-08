293b4850dc1379eddf2408a6d7f8da54d92a3e80
JHUNIOR <202202131@est.umss.edu>
2024-11-08 17:01:05 -0400

    listar materiales

--- frontend/src/pages/PublicProfile.tsx
+++ frontend/src/pages/PublicProfile.tsx
@@ -1,7 +1,7 @@
 import imgEjemploPerfil from "../assets/ejemploPerfil.jpg";
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import SimpleNavBar from "../components/simpleNavBar"
+import SimpleNavBar from "../components/simpleNavBar";
 import material1 from "../assets/material1.png";
 import material2 from "../assets/material2.png";
 import material3 from "../assets/material3.png";
@@ -10,6 +10,7 @@ import material5 from "../assets/material5.png";
 import { MaterialProp } from "../components/Material";
 import { Link } from "react-router-dom";
 import ListarPropios from "../components/ListarPropios";
+import { URL_BACKEND } from "../constant/url";
 const Page = () => {
     const [nombre_usuario, setNombre] = useState("");
     //const [numero_telefono, setTelefono] = useState("");
@@ -20,6 +21,25 @@ const Page = () => {
     // Ejemplo de datos; estos valores deberían venir del backend.
     const [propios, setPropios] = useState<MaterialProp[]>([]);
 
+    const getMaterialesPropios = async () => {
+        const user =
+            (await localStorage.getItem("user")) ||
+            (await sessionStorage.getItem("user")) ||
+            null;
+        if (user) {
+            const { id_usuario } = JSON.parse(user);
+            const response = await fetch(
+                `${URL_BACKEND}/api/materiales/usuario/${id_usuario}`
+            );
+            const data = await response.json();
+            if (response.ok) {
+                console.log("materiales",data.data);
+                setPropios(data.data)
+            } else {
+                console.error("Error al obtener los materiales propios", data);
+            }
+        }
+    };
     //Esta lista reemplazar por la API de similares
     useEffect(() => {
         // esta lista debe ser reemplazada por la API de recomendados
@@ -58,6 +78,7 @@ const Page = () => {
         ];
 
         setPropios(propios);
+        getMaterialesPropios();
     }, []);
 
     useEffect(() => {
@@ -65,7 +86,6 @@ const Page = () => {
             localStorage.getItem("user") ||
             sessionStorage.getItem("user") ||
             null;
-        console.log(data)
         if (data) {
             const user = JSON.parse(data);
             console.log(user);
@@ -76,8 +96,6 @@ const Page = () => {
             setImagenPerfil(user.imagen_perfil || imgEjemploPerfil);
         }
     }, []);
-
-
 
     return (
         <>
@@ -146,12 +164,21 @@ const Page = () => {
 
                                     <div className="flex flex-col space-y-2 ml-8 -mt-10 mb-10">
                                         {/* Nombre */}
-                                        <h1 className="font-bold text-2xl whitespace-nowrap">{nombre_usuario}</h1>
+                                        <h1 className="font-bold text-2xl whitespace-nowrap">
+                                            {nombre_usuario}
+                                        </h1>
 
                                         {/* Ubicación */}
                                         <div className="mt-4 sm:mt-0 flex items-center space-x-2">
-                                            <svg xmlns="http://www.w3.org/2000/svg" className="w-5 h-5" viewBox="0 0 384 512">
-                                                <path fill="#FFD43B" d="M215.7 499.2C267 435 384 279.4 384 192C384 86 298 0 192 0S0 86 0 192c0 87.4 117 243 168.3 307.2c12.3 15.3 35.1 15.3 47.4 0zM192 128a64 64 0 1 1 0 128 64 64 0 1 1 0-128z" />
+                                            <svg
+                                                xmlns="http://www.w3.org/2000/svg"
+                                                className="w-5 h-5"
+                                                viewBox="0 0 384 512"
+                                            >
+                                                <path
+                                                    fill="#FFD43B"
+                                                    d="M215.7 499.2C267 435 384 279.4 384 192C384 86 298 0 192 0S0 86 0 192c0 87.4 117 243 168.3 307.2c12.3 15.3 35.1 15.3 47.4 0zM192 128a64 64 0 1 1 0 128 64 64 0 1 1 0-128z"
+                                                />
                                             </svg>
                                             <label className="text-sm hover:underline hover:text-blue-400 text-gray-700">
                                                 {zona_trabajo}
@@ -161,8 +188,15 @@ const Page = () => {
                                         {/* Calificación */}
                                         <div className="flex items-center space-x-2">
                                             {/* Ícono Estrella */}
-                                            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 576 512">
-                                                <path fill="#FFD43B" d="M316.9 18C311.6 7 300.4 0 288.1 0s-23.4 7-28.8 18L195 150.3 51.4 171.5c-12 1.8-22 10.2-25.7 21.7s-.7 24.2 7.9 32.7L137.8 329 113.2 474.7c-2 12 3 24.2 12.9 31.3s23 8 33.8 2.3l128.3-68.5 128.3 68.5c10.8 5.7 23.9 4.9 33.8-2.3s14.9-19.3 12.9-31.3L438.5 329 542.7 225.9c8.6-8.5 11.7-21.2 7.9-32.7s-13.7-19.9-25.7-21.7L381.2 150.3 316.9 18z" />
+                                            <svg
+                                                xmlns="http://www.w3.org/2000/svg"
+                                                className="h-5 w-5"
+                                                viewBox="0 0 576 512"
+                                            >
+                                                <path
+                                                    fill="#FFD43B"
+                                                    d="M316.9 18C311.6 7 300.4 0 288.1 0s-23.4 7-28.8 18L195 150.3 51.4 171.5c-12 1.8-22 10.2-25.7 21.7s-.7 24.2 7.9 32.7L137.8 329 113.2 474.7c-2 12 3 24.2 12.9 31.3s23 8 33.8 2.3l128.3-68.5 128.3 68.5c10.8 5.7 23.9 4.9 33.8-2.3s14.9-19.3 12.9-31.3L438.5 329 542.7 225.9c8.6-8.5 11.7-21.2 7.9-32.7s-13.7-19.9-25.7-21.7L381.2 150.3 316.9 18z"
+                                                />
                                             </svg>
 
                                             {/* Calificación */}
@@ -174,8 +208,15 @@ const Page = () => {
                                         {/* Botón Contactar */}
                                         <div className="mt-2">
                                             <button className="flex items-center text-white bg-yellow-400 rounded-md px-2 py-1 space-x-1">
-                                                <svg xmlns="http://www.w3.org/2000/svg" className="w-5 h-5" viewBox="0 0 512 512">
-                                                    <path fill="#ffffff" d="M160 368c26.5 0 48 21.5 48 48l0 16 72.5-54.4c8.3-6.2 18.4-9.6 28.8-9.6L448 368c8.8 0 16-7.2 16-16l0-288c0-8.8-7.2-16-16-16L64 48c-8.8 0-16 7.2-16 16l0 288c0 8.8 7.2 16 16 16l96 0zm48 124l-.2 .2-5.1 3.8-17.1 12.8c-4.8 3.6-11.3 4.2-16.8 1.5s-8.8-8.2-8.8-14.3l0-21.3 0-6.4 0-.3 0-4 0-48-48 0-48 0c-35.3 0-64-28.7-64-64L0 64C0 28.7 28.7 0 64 0L448 0c35.3 0 64 28.7 64 64l0 288c0 35.3-28.7 64-64 64l-138.7 0L208 492z" />
+                                                <svg
+                                                    xmlns="http://www.w3.org/2000/svg"
+                                                    className="w-5 h-5"
+                                                    viewBox="0 0 512 512"
+                                                >
+                                                    <path
+                                                        fill="#ffffff"
+                                                        d="M160 368c26.5 0 48 21.5 48 48l0 16 72.5-54.4c8.3-6.2 18.4-9.6 28.8-9.6L448 368c8.8 0 16-7.2 16-16l0-288c0-8.8-7.2-16-16-16L64 48c-8.8 0-16 7.2-16 16l0 288c0 8.8 7.2 16 16 16l96 0zm48 124l-.2 .2-5.1 3.8-17.1 12.8c-4.8 3.6-11.3 4.2-16.8 1.5s-8.8-8.2-8.8-14.3l0-21.3 0-6.4 0-.3 0-4 0-48-48 0-48 0c-35.3 0-64-28.7-64-64L0 64C0 28.7 28.7 0 64 0L448 0c35.3 0 64 28.7 64 64l0 288c0 35.3-28.7 64-64 64l-138.7 0L208 492z"
+                                                    />
                                                 </svg>
                                                 <span>Contacto</span>
                                             </button>
@@ -196,7 +237,10 @@ const Page = () => {
                                 Catálogo de venta
                             </h2>
                             <Link to="/propios">
-                                <a href="#ver-todo" className="text-[#FDBC3F] hover:underline">
+                                <a
+                                    href="#ver-todo"
+                                    className="text-[#FDBC3F] hover:underline"
+                                >
                                     Ver Todo
                                 </a>
                             </Link>
@@ -205,7 +249,6 @@ const Page = () => {
                     </div>
                 </main>
             </div>
-
         </>
     );
 };
